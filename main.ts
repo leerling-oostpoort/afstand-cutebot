d940152fb17e5d6c5cbf73e0a669abc03dab00db
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-12-19 11:57:36 +0100

    achteruit rijden

--- main.ts
+++ main.ts
@@ -8,6 +8,9 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 3) {
         cuteBot.turnright()
     }
+    if (receivedNumber == 4) {
+        cuteBot.moveTime(cuteBot.Direction.backward, 25, 2)
+    }
 })
 radio.setGroup(1)
 basic.forever(function () {
